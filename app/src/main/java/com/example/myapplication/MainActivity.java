dc3f5498cff821e0f518ad9f898f87fc8b468cb3
raphael1063 <50977787+raphael1063@users.noreply.github.com>
2019-11-12 17:46:09 +0900

    studying git

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-//hfhf
         pauseRestart.setVisibility(View.GONE);
     }
 
